1bf0a07628190609838c8ecbbf1287796fb03e39
Merciless-S <2051183268yjz@gmail.com>
2019-12-22 13:30:23 -0500

    Keyboard input allowed

--- src/CalculatorView1.java
+++ src/CalculatorView1.java
@@ -2,6 +2,7 @@
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import javax.swing.JButton;
@@ -10,7 +11,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
-public class CalculatorView1 extends JFrame implements CalculatorView {
+public class CalculatorView1 extends JFrame implements CalculatorView, KeyListener {
 
     private CalculatorController controller;
 
@@ -32,7 +33,9 @@ public class CalculatorView1 extends JFrame implements CalculatorView {
     public CalculatorView1(){
         super("Merciless's Calculator");
 
+
         this.tTop = new JTextArea("", TEXT_AREA_HEIGHT, TEXT_AREA_WIDTH);
+        //this.tTop.addKeyListener(this);
         this.tBottom = new JTextArea("", TEXT_AREA_HEIGHT, TEXT_AREA_WIDTH);
         this.bClear = new JButton("Clear");
         this.bEnter = new JButton("Enter");
@@ -114,6 +117,7 @@ public class CalculatorView1 extends JFrame implements CalculatorView {
 
         for (JButton b : this.bDigits) {
             b.addActionListener(this);
+            b.addKeyListener(this);
         }
         this.bClear.addActionListener(this);
         this.bTran.addActionListener(this);
@@ -130,6 +134,24 @@ public class CalculatorView1 extends JFrame implements CalculatorView {
         this.bLeftArrow.addActionListener(this);
         this.bRightArrow.addActionListener(this);
         this.bDot.addActionListener(this);
+        this.tTop.addKeyListener(this);
+        this.tBottom.addKeyListener(this);
+        this.bClear.addKeyListener(this);
+        this.bTran.addKeyListener(this);
+        this.bEnter.addKeyListener(this);
+        this.bAdd.addKeyListener(this);
+        this.bSubtract.addKeyListener(this);
+        this.bMultiply.addKeyListener(this);
+        this.bDivide.addKeyListener(this);
+        this.bPower.addKeyListener(this);
+        this.bRoot.addKeyListener(this);
+        this.bDelete.addKeyListener(this);
+        this.bLeftParen.addKeyListener(this);
+        this.bRightParen.addKeyListener(this);
+        this.bLeftArrow.addKeyListener(this);
+        this.bRightArrow.addKeyListener(this);
+        this.bDot.addKeyListener(this);
+
         this.pack();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
@@ -169,7 +191,6 @@ public class CalculatorView1 extends JFrame implements CalculatorView {
         } else if (source == this.bDivide) {
             this.controller.processEditEvent('/');
         } else if (source == this.bLeftParen) {
-            //System.out.println("pressed");
             this.controller.processEditEvent('(');
         } else if (source == this.bRightParen) {
             this.controller.processEditEvent(')');
@@ -188,10 +209,56 @@ public class CalculatorView1 extends JFrame implements CalculatorView {
         }else{
             for(int i = 0; i < DIGIT_BUTTONS; ++i){
                 if(source == this.bDigits[i]){
-                    //System.out.println((char)(i + '0'));
                     this.controller.processEditEvent((char)(i + '0'));
+                    break;
                 }
             }
         }
     }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+        if(key == KeyEvent.VK_LEFT){
+            this.controller.processShiftEvent(-1);
+        }else if(key == KeyEvent.VK_RIGHT){
+            this.controller.processShiftEvent(1);
+        }else if(key == KeyEvent.VK_ENTER){
+            this.controller.processEnterEvent();
+        }else if(key == KeyEvent.VK_MINUS){
+            this.controller.processEditEvent('-');
+        }else if(key == KeyEvent.VK_EQUALS && e.isShiftDown()){
+            this.controller.processEditEvent('+');
+        }else if(key == KeyEvent.VK_SLASH){
+            this.controller.processEditEvent('/');
+        }else if(key == KeyEvent.VK_8 && e.isShiftDown()){
+            this.controller.processEditEvent('*');
+        }else if(key == KeyEvent.VK_9 && e.isShiftDown()){
+            this.controller.processEditEvent('(');
+        }else if(key == KeyEvent.VK_0 && e.isShiftDown()){
+            this.controller.processEditEvent(')');
+        } else if(key == KeyEvent.VK_BACK_SPACE){
+            this.controller.processDeleteEvent();
+        }else if(key == KeyEvent.VK_PERIOD){
+            this.controller.processEditEvent('.');
+        }else if(key == KeyEvent.VK_C && !e.isControlDown()){
+            //this.controller.processClearEvent();
+        }else if(key == KeyEvent.VK_T && this.bTran.isEnabled()){
+            this.controller.processTranEvent();
+        }else{
+            for(int i = 0; i < DIGIT_BUTTONS; i++){
+                if(key == i + KeyEvent.VK_0){
+                    this.controller.processEditEvent((char)(i + '0'));
+                    break;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) { }
 }
